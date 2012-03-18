e4154f5d30d2e484a064a9e0ba7e1436fa6a9f6a
songshan99 <songshan99@gmail.com>
2012-03-18 12:08:09 +0000

    Code framework almost finished

---
+++ android-ftdi-usb2serial-driver-package/src/shansong/ftdi/d2xx/FTDI_EEPROM.java
@@ -0,0 +1,27 @@
+/*
+ * This is the code file that defines the class to represent the EEPROM attached to a FTDI device.
+ * All EEPROM related data and functions are defined here.
+ */
+package shansong.ftdi.d2xx;
+
+/**
+ * The Class FTDI_EEPROM.
+ */
+public class FTDI_EEPROM {
+	//==================================================================
+	//	1.command codes for FTDI chip EEPROM operations
+	//==================================================================
+	
+	//==================================================================
+	//	2. member objects
+	//==================================================================
+
+	//==================================================================
+	//	3. The section of member methods
+	//==================================================================
+	//readEEPROM, writeEEPROM, eraseEEPROM
+	//readChipID
+	//readEEPROMLocation, writeEEPROMLocation
+	
+	//TODO: i believe we also need some more EEPROM tooling functions here.
+}
