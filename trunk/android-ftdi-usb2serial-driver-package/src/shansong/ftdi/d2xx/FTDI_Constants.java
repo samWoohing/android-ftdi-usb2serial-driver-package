c5b7c9c54866ad9ecb2c56526c5a04940821ac6b
songshan99@gmail.com <songshan99@gmail.com@5b17d186-6de7-108e-be69-755b2835a177>
2012-03-19 15:21:48 +0000

    Add more constants into FTDI_Constants Further developed FTDI_Device and FTDI_Interface. Prefer to have a separate init function, rather than doing everything in the constructors.

--- trunk/android-ftdi-usb2serial-driver-package/src/shansong/ftdi/d2xx/FTDI_Constants.java
+++ trunk/android-ftdi-usb2serial-driver-package/src/shansong/ftdi/d2xx/FTDI_Constants.java
@@ -7,33 +7,73 @@ package shansong.ftdi.d2xx;
 import android.hardware.usb.UsbConstants;
 
 public final class FTDI_Constants {
-
-	///* Definitions for flow control */
-	//#define SIO_RESET          0 /* Reset the port */
-	//#define SIO_MODEM_CTRL     1 /* Set the modem control register */
-	//#define SIO_SET_FLOW_CTRL  2 /* Set flow control register */
-	//#define SIO_SET_BAUD_RATE  3 /* Set baud rate */
-	//#define SIO_SET_DATA       4 /* Set the data characteristics of the port */
-
-	///* Requests */
-	//#define SIO_RESET_REQUEST             SIO_RESET
-	//#define SIO_SET_BAUDRATE_REQUEST      SIO_SET_BAUD_RATE
-	//#define SIO_SET_DATA_REQUEST          SIO_SET_DATA
-	//#define SIO_SET_FLOW_CTRL_REQUEST     SIO_SET_FLOW_CTRL
-	//#define SIO_SET_MODEM_CTRL_REQUEST    SIO_MODEM_CTRL
-	//#define SIO_POLL_MODEM_STATUS_REQUEST 0x05
-	//#define SIO_SET_EVENT_CHAR_REQUEST    0x06
-	//#define SIO_SET_ERROR_CHAR_REQUEST    0x07
-	//#define SIO_SET_LATENCY_TIMER_REQUEST 0x09
-	//#define SIO_GET_LATENCY_TIMER_REQUEST 0x0A
-	//#define SIO_SET_BITMODE_REQUEST       0x0B
-	//#define SIO_READ_PINS_REQUEST         0x0C
-	//#define SIO_READ_EEPROM_REQUEST       0x90
-	//#define SIO_WRITE_EEPROM_REQUEST      0x91
-	//#define SIO_ERASE_EEPROM_REQUEST      0x92
+	//The Android usb control message transfer function is defined as:
+	//public int controlTransfer (int requestType, int request, int value, int index, byte[] buffer, int length, int timeout) 
+	//So the requestType, request, value, index are all used to send control messages. Below are some pre-defined values.
 	
-	private static final int USB_RECIP_DEVICE = 0x00;
+	/* Pre-defined RequestType: */
+	//FTDI usb-to-serial chips only use 2 request types. In request and out request:
+	public static final int USB_RECIP_DEVICE = 0x00;
+	public static final int FTDI_DEVICE_OUT_REQTYPE = UsbConstants.USB_TYPE_VENDOR|USB_RECIP_DEVICE|UsbConstants.USB_DIR_OUT;
+	public static final int FTDI_DEVICE_IN_REQTYPE = UsbConstants.USB_TYPE_VENDOR|USB_RECIP_DEVICE|UsbConstants.USB_DIR_IN ;
+	
+	/* Pre-defined Requests:  */	
+	public static final int SIO_RESET_REQUEST				=0x00;
+	public static final int SIO_SET_MODEM_CTRL_REQUEST		=0x01;
+	public static final int SIO_SET_FLOW_CTRL_REQUEST		=0x02;
+	public static final int SIO_SET_BAUDRATE_REQUEST		=0x03;
+	public static final int SIO_SET_DATA_REQUEST			=0x04;	
+	public static final int SIO_POLL_MODEM_STATUS_REQUEST	=0x05;
+	public static final int SIO_SET_EVENT_CHAR_REQUEST		=0x06;
+	public static final int SIO_SET_ERROR_CHAR_REQUEST		=0x07;
+	public static final int SIO_SET_LATENCY_TIMER_REQUEST	=0x09;
+	public static final int SIO_GET_LATENCY_TIMER_REQUEST	=0x0A;
+	public static final int SIO_SET_BITMODE_REQUEST			=0x0B;
+	public static final int SIO_READ_PINS_REQUEST			=0x0C;
+	public static final int SIO_READ_EEPROM_REQUEST			=0x90;
+	public static final int SIO_WRITE_EEPROM_REQUEST		=0x91;
+	public static final int SIO_ERASE_EEPROM_REQUEST		=0x92;
+	
+	/* pre-defined Values: */
+	//Note: these 3 values are always used with SIO_RESET_REQUEST
+	public static final int SIO_RESET_SIO			=0x00;
+	public static final int SIO_RESET_PURGE_RX		=0x01;
+	public static final int SIO_RESET_PURGE_TX		=0x02;
 
-	private static final int FTDI_DEVICE_OUT_REQTYPE = UsbConstants.USB_TYPE_VENDOR|USB_RECIP_DEVICE|UsbConstants.USB_DIR_OUT;
-	private static final int FTDI_DEVICE_IN_REQTYPE = UsbConstants.USB_TYPE_VENDOR|USB_RECIP_DEVICE|UsbConstants.USB_DIR_IN ;
+	//Note: for set DTR and RTS, the *_HIGH and *_LOW sets the high and low value.
+	//the controlTransfer input parameter "index" represents the port A,B,C or D.
+	public static final int SIO_SET_DTR_MASK		=0x1;
+	public static final int SIO_SET_DTR_HIGH		=( 1 | ( SIO_SET_DTR_MASK  << 8));
+	public static final int SIO_SET_DTR_LOW 		=( 0 | ( SIO_SET_DTR_MASK  << 8));
+	public static final int SIO_SET_RTS_MASK		=0x2;
+	public static final int SIO_SET_RTS_HIGH		=( 2 | ( SIO_SET_RTS_MASK << 8 ));
+	public static final int SIO_SET_RTS_LOW			=( 0 | ( SIO_SET_RTS_MASK << 8 ));
+	
+	/* pre-defined Index: */
+	//The FTDI channel(interface) is the most commonly used as Index parameter in usb control message tranmitting.
+	public static final int INTERFACE_ANY = 0;
+	public static final int INTERFACE_A = 1;
+	public static final int INTERFACE_B = 2;
+	public static final int INTERFACE_C = 3;
+	public static final int INTERFACE_D = 4;
+	
+	//Note: these are used together with SIO_SET_FLOW_CTRL_REQUEST.
+	//flow control setting is a little special, the following values are actually sent in "Index"
+	//while higher byte is flow control, lower byte is chip channel index (channel A, B, C or D)
+	public static final int SIO_DISABLE_FLOW_CTRL	=0x0;
+	public static final int SIO_RTS_CTS_HS			=(0x1 << 8);
+	public static final int SIO_DTR_DSR_HS			=(0x2 << 8);
+	public static final int SIO_XON_XOFF_HS			=(0x4 << 8);
+	
+	
+	/*Endpoint address for each interface (A, B, C and D)*/
+	public static final int INTERFACE_A_ENDPOINT_IN 	= 0x02;
+	public static final int INTERFACE_A_ENDPOINT_OUT 	= 0x81;
+	public static final int INTERFACE_B_ENDPOINT_IN 	= 0x04;
+	public static final int INTERFACE_B_ENDPOINT_OUT 	= 0x83;
+	public static final int INTERFACE_C_ENDPOINT_IN 	= 0x06;
+	public static final int INTERFACE_C_ENDPOINT_OUT 	= 0x85;
+	public static final int INTERFACE_D_ENDPOINT_IN 	= 0x08;
+	public static final int INTERFACE_D_ENDPOINT_OUT	= 0x87;
+	
 }
--- trunk/android-ftdi-usb2serial-driver-package/src/shansong/ftdi/d2xx/FTDI_Interface.java
+++ trunk/android-ftdi-usb2serial-driver-package/src/shansong/ftdi/d2xx/FTDI_Interface.java
@@ -7,6 +7,7 @@ package shansong.ftdi.d2xx;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbConstants;
 
 /**
  * The Class FTDI_Interface.
@@ -37,7 +38,15 @@ public class FTDI_Interface {
 	/** The Usb time out setting. it'll be used by usbBulkTransfer or usbControlTransfer. 
 	 * But the question is... shall we maintain different timeout value for each?? */
 	private int mUsbTimeOut;
-	//TODO: decide if we need separated timeout value setting later.
+	
+	/** The class shall know which interface itself is, A, B, C or D?. */
+	private int mInterface;
+	
+	/** The read timeout. */
+	private int mReadTimeout	=5000;
+	
+	/** The write timeout. */
+	private int mWriteTimeout	=5000;
 	
 	//==================================================================
 	//	3. The section of member methods
@@ -47,13 +56,22 @@ public class FTDI_Interface {
 	 *
 	 * @param newUsbInterface: the usb interface that we'd like to create a FTDI_interface on. It shall be given by FTDI_Device.
 	 */
-	public FTDI_Interface(UsbInterface newUsbInterface)
+	public FTDI_Interface()
 	{
-		mUsbInterface = newUsbInterface;
-		
-		//verify if this interface has 2 endpoints, one is reading and one is writing. If else, it's NOT a FTDI channel endpoint.
+		//TODO: anything else
 	}
 	
+	public int initInterface(UsbInterface newUsbInterface)
+	{
+		//verify if this interface has 2 endpoints, one is reading and one is writing. If else, it's NOT a FTDI channel endpoint.
+		//give mInterface the proper value.
+		
+		//give mEndpointRead and mEndpointWrite the proper value.
+		
+		//finally the interface is OK.
+		mUsbInterface = newUsbInterface;
+		return 0;
+	}
 	/**
 	 * Sets the usb device connection.  This method shall be used by FTDI_Device when open the device.
 	 *
@@ -72,6 +90,60 @@ public class FTDI_Interface {
 		mUsbDeviceConnection = null;
 	}
 	
+	/**
+	 * Recognize interface is A, B C or D by its endpoint type.
+	 *
+	 * @param newUsbInterface: the usb interface 
+	 * @return INTERFACE_A, B, C or D. -1 if the endpoint is not recognized.
+	 */
+	private int recognizeInterfaceByEndpoint(UsbInterface newUsbInterface)
+	{
+		//verify only 2 endpoints on the interface
+		int numOfEndpoint = newUsbInterface.getEndpointCount();
+		if(numOfEndpoint!=2)
+		{
+			return -1;
+		}
+		
+		//verify the 2 endpoints is one input and one output
+		UsbEndpoint ep1 = newUsbInterface.getEndpoint(0);
+		UsbEndpoint ep2 = newUsbInterface.getEndpoint(1);
+		UsbEndpoint epIn, epOut;
+		if(ep1.getDirection() == UsbConstants.USB_DIR_IN && ep2.getDirection() == UsbConstants.USB_DIR_OUT)
+		{
+			epIn = ep1;
+			epOut = ep2;
+		}
+		else if(ep1.getDirection() == UsbConstants.USB_DIR_OUT && ep2.getDirection() == UsbConstants.USB_DIR_IN)
+		{
+			epIn = ep2;
+			epOut = ep1;
+		}
+		else
+		{
+			return -1;
+		}
+		
+		//check by endpoint address
+		if(epIn.getAddress()==FTDI_Constants.INTERFACE_A_ENDPOINT_IN && epOut.getAddress()==FTDI_Constants.INTERFACE_A_ENDPOINT_OUT)
+		{
+			return FTDI_Constants.INTERFACE_A;
+		}
+		if(epIn.getAddress()==FTDI_Constants.INTERFACE_B_ENDPOINT_IN && epOut.getAddress()==FTDI_Constants.INTERFACE_B_ENDPOINT_OUT)
+		{
+			return FTDI_Constants.INTERFACE_B;
+		}
+		if(epIn.getAddress()==FTDI_Constants.INTERFACE_C_ENDPOINT_IN && epOut.getAddress()==FTDI_Constants.INTERFACE_C_ENDPOINT_OUT)
+		{
+			return FTDI_Constants.INTERFACE_C;
+		}
+		if(epIn.getAddress()==FTDI_Constants.INTERFACE_D_ENDPOINT_IN && epOut.getAddress()==FTDI_Constants.INTERFACE_D_ENDPOINT_OUT)
+		{
+			return FTDI_Constants.INTERFACE_D;
+		}
+		
+		return -1;
+	}
 	//==================================================================
 	//	3.x Serial port control and operation methods
 	//==================================================================
