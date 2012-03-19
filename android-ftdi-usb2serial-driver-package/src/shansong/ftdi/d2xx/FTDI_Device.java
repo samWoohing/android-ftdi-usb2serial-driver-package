add531cfe3ace5919121139c9604566327b10e57
songshan99 <songshan99@gmail.com>
2012-03-19 15:21:48 +0000

    Add more constants into FTDI_Constants Further developed FTDI_Device and FTDI_Interface. Prefer to have a separate init function, rather than doing everything in the constructors.

--- android-ftdi-usb2serial-driver-package/src/shansong/ftdi/d2xx/FTDI_Device.java
+++ android-ftdi-usb2serial-driver-package/src/shansong/ftdi/d2xx/FTDI_Device.java
@@ -11,7 +11,6 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.hardware.usb.UsbConstants;
 
-
 // TODO: Auto-generated Javadoc
 /**
  * The Class FTDI_Device.
@@ -47,6 +46,11 @@ public class FTDI_Device{
 	/** The ftdi eeprom. One FTDI device(chip) shall only have 1 eeprom*/
 	private FTDI_EEPROM mFTDI_EEPROM;
 	
+	/** The read timeout. */
+	private int mReadTimeout	=5000;
+	
+	/** The write timeout. */
+	private int mWriteTimeout	=5000;
 	//==================================================================
 	//	3. The section of member methods
 	//==================================================================
@@ -55,6 +59,7 @@ public class FTDI_Device{
 	 *
 	 * @param dev the dev
 	 */
+	//TODO: shall we give a separated constructor and init function for both FTDI_Device and FTDI_Interface?? Think!
 	public FTDI_Device(Context context, UsbDevice dev)
 	{
 		//Write down the caller Activity or Service's context
@@ -96,42 +101,64 @@ public class FTDI_Device{
 	//==================================================================
 	
 	//openDevice, closeDevice, resetDevice
-	int openDevice()
+	public int openDevice()
 	{
 		UsbManager mManager = (UsbManager)mContext.getSystemService(Context.USB_SERVICE);
 		//check if we have the permission to this device
-		if(mManager.hasPermission(mUsbDevice))
-		{
-			mUsbDeviceConnection = mManager.openDevice(mUsbDevice);
-			return 0;
-		}
-		else
+		if(!mManager.hasPermission(mUsbDevice))
 		{
 			//write the permission problem to Android log, and return -1
 			Log.e(TAG,"Permission denied when opening the device:"+mUsbDevice.toString());
 			return -1;
 		}
+		
+		mUsbDeviceConnection = mManager.openDevice(mUsbDevice);
+		//TODO: consider if we need to reset the device, purge TX/RX buffer.
+		if(resetDevice()!=0)
+		{
+			Log.e(TAG,"Failed to reset device:"+mUsbDevice.toString());
+			return -1;
+		}
+		if(usbPurgeRXBuffer()!=0)
+		{
+			Log.e(TAG,"Failed to purge RX buffer on device:"+mUsbDevice.toString());
+			return -1;
+		}
+		if(usbPurgeTXBuffer()!=0)
+		{
+			Log.e(TAG,"Failed to purge TX buffer on device:"+mUsbDevice.toString());
+			return -1;
+		}
+		//arrive here then everything is fine.
+		Log.d(TAG,"USB device opened:"+mUsbDevice.toString());
+		return 0;
 	}
 	
-	void closeDevice()
+	public void closeDevice()
 	{
 		mUsbDeviceConnection.close();
 		mUsbDeviceConnection = null;
 	}
 	
-	int resetDevice()
+	//TODO: decide if we shall define these 3 resetting functions as private.
+	private int resetDevice()
 	{
-		//mUsbDeviceConnection.controlTransfer(requestType, request, value, index, buffer, length, timeout);
-		return 0;//TODO: detailed implementation. set reset command through USB control message
+		return mUsbDeviceConnection.controlTransfer(FTDI_Constants.FTDI_DEVICE_OUT_REQTYPE, FTDI_Constants.SIO_RESET_REQUEST, 
+				FTDI_Constants.SIO_RESET_SIO, FTDI_Constants.INTERFACE_ANY, null, 0, mWriteTimeout);
+		//TODO: I give it a INTERFACE_ANY as parameter. Need to verify if it is correct. I believe the Index doesn't matter.
 	}
 	
 	//usbPurgeRXBuffer, usbPurgeTXBuffer, Hmm... maybe this shall be defined as private functions just for internal use only?
-	int usbPurgeRXBuffer()
+	private int usbPurgeRXBuffer()
 	{
-		return 0;//TODO: detailed implementation
+		return mUsbDeviceConnection.controlTransfer(FTDI_Constants.FTDI_DEVICE_OUT_REQTYPE, FTDI_Constants.SIO_RESET_PURGE_RX,
+				FTDI_Constants.SIO_RESET_SIO, FTDI_Constants.INTERFACE_ANY, null, 0, mWriteTimeout);
+		//TODO: I give it a INTERFACE_ANY as parameter. Need to verify if it is correct. I believe the Index doesn't matter.
 	}
-	int usbPurgeTXBuffer()
+	private int usbPurgeTXBuffer()
 	{
-		return 0;//TODO: detailed implementation
+		return mUsbDeviceConnection.controlTransfer(FTDI_Constants.FTDI_DEVICE_OUT_REQTYPE, FTDI_Constants.SIO_RESET_PURGE_TX, 
+				FTDI_Constants.SIO_RESET_SIO, FTDI_Constants.INTERFACE_ANY, null, 0, mWriteTimeout);
+		//TODO: I give it a INTERFACE_ANY as parameter. Need to verify if it is correct. I believe the Index doesn't matter.
 	}
 }
