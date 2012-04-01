0eefc43a680f653183273925fe79cf3df087f45f
songshan99 <songshan99@gmail.com>
2012-04-01 09:31:20 +0000

    Well, I decided to use ExpandableListView for port configuration window. Let's keep going.

--- android-ftdi-usb2serial-driver-package/gen/shansong/ftdi/SandboxApp/R.java
+++ android-ftdi-usb2serial-driver-package/gen/shansong/ftdi/SandboxApp/R.java
@@ -14,7 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f050003;
+        public static final int button2=0x7f050004;
+        public static final int editText1=0x7f050005;
+        public static final int linearLayout1=0x7f050001;
+        public static final int linearLayout2=0x7f050008;
+        public static final int spinner1=0x7f050007;
+        public static final int spinner2=0x7f05000a;
         public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050006;
+        public static final int textView3=0x7f050009;
+        public static final int textView4=0x7f05000b;
+        public static final int toggleButton1=0x7f050002;
     }
     public static final class layout {
         public static final int hyper_term_main=0x7f030000;
