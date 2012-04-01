326d77bcebbfad0960900a9ccfe0d6ca7aba9ed6
songshan99@gmail.com <songshan99@gmail.com@5b17d186-6de7-108e-be69-755b2835a177>
2012-04-01 06:10:03 +0000

    Commit adding HyperTermMainFrag.java and SerialPortConfigFrag.java.

---
+++ trunk/android-ftdi-usb2serial-driver-package/src/shansong/ftdi/SandboxApp/SerialPortConfigFrag.java
@@ -0,0 +1,28 @@
+package shansong.ftdi.SandboxApp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SerialPortConfigFrag extends Fragment {
+	/**
+     * When creating, TODO: decide what to do
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    /**
+     * The Fragment's UI is loaded in onCreateView
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.serial_port_config, container, false);
+        //TODO: put any GUI initialization needed into here
+        return v;
+    }
+}
