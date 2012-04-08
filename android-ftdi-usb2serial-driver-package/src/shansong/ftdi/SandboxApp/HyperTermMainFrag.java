fdff64915c32e684493d5ed8336a947da0c46ffb
songshan99 <songshan99@gmail.com>
2012-04-08 10:28:57 +0000

    Added FTDI_SerialPortReadWriteTask. Let's put the task as a standalone code rather than embed it into HyperTermMainFrag.java, for future reuse purpose. And also we should consider another layer over D2xx library, to implement serial port functionalities.

--- android-ftdi-usb2serial-driver-package/src/shansong/ftdi/SandboxApp/HyperTermMainFrag.java
+++ android-ftdi-usb2serial-driver-package/src/shansong/ftdi/SandboxApp/HyperTermMainFrag.java
@@ -1,7 +1,10 @@
 package shansong.ftdi.SandboxApp;
 
+import java.nio.ByteBuffer;
+
 import android.app.Fragment;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,19 +13,31 @@ import android.view.View.OnClickListener;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 import android.widget.Spinner;
+import shansong.ftdi.d2xx.FTDI_Interface;
 
-
+// TODO: Auto-generated Javadoc
+/**
+ * The Class HyperTermMainFrag.
+ */
 public class HyperTermMainFrag extends Fragment {
+	
 	/**
-     * When creating, TODO: decide what to do
-     */
+	 * When creating, TODO: decide what to do.
+	 *
+	 * @param savedInstanceState the saved instance state
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
     
     /**
-     * The Fragment's UI is loaded in onCreateView
+     * The Fragment's UI is loaded in onCreateView.
+     *
+     * @param inflater the inflater
+     * @param container the container
+     * @param savedInstanceState the saved instance state
+     * @return the view
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,12 +54,16 @@ public class HyperTermMainFrag extends Fragment {
         return v;
     }
     
+    /** The button click listener of the "connect" button. */
     private OnClickListener mConnectBtnListener = new OnClickListener() {
         public void onClick(View v) {
             // use following example to get the current activity:
+        	//TODO: detailed implementation. Currently just dummy code.
         	Toast.makeText(getActivity(), "Connect button clicked!", Toast.LENGTH_SHORT).show();
         	SandboxAppActivity mApp = (SandboxAppActivity)getActivity();
         	mApp.mIsSerialPortConnected = mApp.mIsSerialPortConnected ? false:true;
         }
     };
+    
+    
 }
