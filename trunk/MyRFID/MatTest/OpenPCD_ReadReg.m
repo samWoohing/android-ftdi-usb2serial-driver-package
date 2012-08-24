b6bad4368be8ed04cd1fc0c326d1e3c92d22178b
songshan99@gmail.com <songshan99@gmail.com@5b17d186-6de7-108e-be69-755b2835a177>
2012-08-24 05:55:53 +0000

    Now we are able to communicate with Mifare card.

--- trunk/MyRFID/MatTest/OpenPCD_ReadReg.m
+++ trunk/MyRFID/MatTest/OpenPCD_ReadReg.m
@@ -1,13 +1,42 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Read a register value from OpenPCD device
+%
+%   [regvalue, result] = OpenPCD_ReadReg(hdl, reg)
+%
+%   regvalue: register value read from RC632
+%   result: returns 0 of no error. 
+%           -1 if write error, 
+%           -2 if read error, 
+%           -3 if flag error
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [regvalue, flag] = OpenPCD_ReadReg(hdl, reg)
+function [regvalue, result] = OpenPCD_ReadReg(hdl, reg)
 EP1W=int32(hex2dec('1'));%write endpoint
 EP2R=int32(hex2dec('82')); %note that read endpoint always begins with 8
 timeout = int32(127);
 
 byteW = int8([22, 0, reg, 0]);%refer to OpenPCD.h for command codes.
-result = libusb_usb_bulk_write(hdl, EP1W, byteW, timeout);
-[byteR, result] = libusb_usb_bulk_read(hdl, EP2R, int32(4), timeout);
-regvalue=byteR(4);
+w_result = libusb_usb_bulk_write(hdl, EP1W, byteW, timeout);
+
+if w_result < 0
+    result = -1;
+    regvalue = int8(0);
+    return;
+end
+
+[byteR, r_result] = libusb_usb_bulk_read(hdl, EP2R, int32(4), timeout);
 flag=byteR(2);
+
+if r_result < 0
+	regvalue = int8(0);
+	result = -2;
+    return;
+end
+
+if flag == 128  %#define OPENPCD_FLAG_ERROR	0x80
+	regvalue = int8(0);
+    result = -3;
+    return;
+end
+%if everything is OK:
+regvalue=byteR(4);
+result = 0;
