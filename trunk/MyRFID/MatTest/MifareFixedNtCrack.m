465d183534b6f9798a490e3197e4c5b670bbed64
songshan99@gmail.com <songshan99@gmail.com@5b17d186-6de7-108e-be69-755b2835a177>
2012-09-04 15:14:27 +0000

    Add test code MifareFixedNtCrack. Not working yet. Need to debug.

---
+++ trunk/MyRFID/MatTest/MifareFixedNtCrack.m
@@ -0,0 +1,35 @@
+function [bytes, result] = MifareFixedNtCrack(hdl, Nr_Ar_Parity)
+EP1W=int32(hex2dec('1'));%write endpoint
+EP2R=int32(hex2dec('82')); %note that read endpoint always begins with 8
+timeout = int32(1024);
+
+params = zeros(1,21, 'uint8');
+params(2)=uint8(1);
+params(12:20)=uint8(Nr_Ar_Parity);
+byteW = [uint8([28, 0, 0, 21]),params];
+
+w_result = libusb_usb_bulk_write(hdl, EP1W, byteW, timeout);
+
+if w_result < 0
+    result = -1;
+    bytes = uint8(0);
+    return;
+end
+
+[byteR, r_result] = libusb_usb_bulk_read(hdl, EP2R, int32(length(byteW)), timeout);
+flag=byteR(2);
+
+if r_result < 0
+	bytes = uint8(0);
+	result = -2;
+    return;
+end
+
+if flag == 128  %#define OPENPCD_FLAG_ERROR	0x80
+	bytes = uint8(0);
+    result = -3;
+    return;
+end
+%if everything is OK:
+bytes=byteR(5:end);
+result = 0;
